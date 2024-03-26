d1d12a3b680f82fa2ab4ee743812bd299f65c9d1
1yoouoo <1yoouoo@gmail.com>
2024-03-26 23:02:29 +0900

    v3

--- src/pages/api/screenshot.ts
+++ src/pages/api/screenshot.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import puppeteer from "puppeteer";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import path from "path";
+import fs from "fs";
 import { transformGameInfo } from "../../../utils/transformToModel";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import GameInfoDto from "@/types/GameInfoDto";
 import { convertJsonToQueryString } from "../../../utils/formatJson";
 
@@ -13,64 +15,90 @@ const s3Client = new S3Client({
   },
 });
 
+const bucketName = process.env.AWS_BUCKET_NAME!;
+const IS_DEVELOPMENT_MODE = process.env.IS_DEVELOPMENT_MODE === "true";
+
 type ResponseData = {
   message: string;
-  screenshotUrl?: string;
-  error?: string;
+  screenshotPath: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (req.method !== "POST") {
-    return res
-      .status(405)
-      .json({ message: "Method Not Allowed", error: "Method Not Allowed" });
-  }
+  if (req.method === "POST") {
+    const gameInfo = req.body as GameInfoDto;
 
-  const gameInfo = req.body as GameInfoDto;
-  const transformedGameInfo = transformGameInfo(gameInfo);
-  const queryString = convertJsonToQueryString(transformedGameInfo);
+    const transformedGameInfo = transformGameInfo(gameInfo);
 
-  try {
+    const queryString = convertJsonToQueryString(transformedGameInfo);
+
     const browser = await puppeteer.launch({
-      headless: true,
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? "/usr/bin/chromium-browser"
+          : undefined,
+      headless: process.env.NODE_ENV !== "development",
     });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 720 });
-    const screenshotUrl = `http://localhost:3000/screenshot?${queryString}`;
-    await page.goto(screenshotUrl, { waitUntil: "networkidle0" });
 
-    const screenshotBuffer = await page.screenshot({
+    const page = await browser.newPage();
+
+    const screenshotUrl = `http://localhost:3000/screenshot?${queryString}`;
+    await page.setViewport({ width: 1280, height: 720 });
+
+    await page.goto(screenshotUrl, {
+      waitUntil: "networkidle0",
+    });
+
+    const assetsDir = path.resolve("assets");
+
+    const deletePrefix = process.env.DELETE_S3_PREFIX;
+
+    if (!fs.existsSync(assetsDir)) {
+      fs.mkdirSync(assetsDir, { recursive: true });
+    }
+
+    const screenshotPath = path.join(
+      assetsDir,
+      `${deletePrefix}-screenshot-${Date.now()}.png`
+    );
+
+    await page.screenshot({
+      path: screenshotPath,
       type: "jpeg",
       quality: 100,
       omitBackground: true,
       clip: { x: 0, y: 0, width: 1280, height: 720 },
     });
 
-    await browser.close();
+    if (IS_DEVELOPMENT_MODE) {
+      await new Promise((resolve) => {
+        console.log(
+          "Server is running in development mode. Press Ctrl+C to stop."
+        );
+      });
+    } else {
+      await browser.close();
+    }
 
-    const screenshotKey = `${
-      process.env.DELETE_S3_PREFIX
-    }-screenshot-${Date.now()}.jpeg`;
-    await s3Client.send(
-      new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME!,
-        Key: screenshotKey,
-        Body: screenshotBuffer,
-      })
-    );
+    const fileStream = fs.createReadStream(screenshotPath);
+
+    const uploadParams = {
+      Bucket: bucketName,
+      Key: screenshotPath,
+      Body: fileStream,
+    };
+
+    const command = new PutObjectCommand(uploadParams);
+    const data = await s3Client.send(command);
+    console.log(`File uploaded successfully. ${data.$metadata.httpStatusCode}`);
 
     res.status(200).json({
       message: "Screenshot taken successfully!",
-      screenshotUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${screenshotKey}`,
+      screenshotPath: `https://${bucketName}.s3.amazonaws.com/${uploadParams.Key}`,
     });
-  } catch (error) {
-    console.error("Error taking or uploading screenshot:", error);
-    res.status(500).json({
-      message: "Failed to take or upload screenshot",
-      error: "Failed to take or upload screenshot",
-    });
+  } else {
+    throw new Error(`Error`);
   }
 }
